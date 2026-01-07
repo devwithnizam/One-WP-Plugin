56db35d3ae8895a27c80512874ea47799a0fa418
programmerNizam <mdnizam12109@gmail.com>
2026-01-07 09:42:30 +0600

    Project metabox shortcode apply

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -67,3 +67,31 @@ add_shortcode('ONE_ATTR_OUTPUT', 'ONE_ATTR_shortcode');
 /**
  *  Project Meta Information Shortcode
  */
+
+function one_wp_project_meta_shortcode($atts)
+{
+    $attrs = shortcode_atts(
+        array(
+            'id' => get_the_ID(),
+        ),
+        $atts,
+        'ONE_PROJECT_META'
+    );
+
+
+    $project_url = get_post_meta($attrs['id'], 'project_url', true);
+    $project_completion = get_post_meta($attrs['id'], 'project_completion_duration', true);
+    $project_cost = get_post_meta($attrs['id'], 'project_estimated_cost', true);
+
+    ob_start();
+?>
+    <div class="one-wp-project-meta" style="border: 1px solid #000; padding: 10px; margin: 10px 0;">
+        <p><strong>Project URL:</strong> <a href="<?php echo esc_url($project_url); ?>"><?php echo esc_url($project_url); ?></a></p>
+        <p><strong>Completion Duration:</strong> <?php echo esc_html($project_completion); ?></p>
+        <p><strong>Estimated Cost:</strong> <?php echo esc_html($project_cost); ?></p>
+    </div>
+<?php
+    return ob_get_clean();
+}
+
+add_shortcode('ONE_PROJECT_META', 'one_wp_project_meta_shortcode');
